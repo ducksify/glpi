e446f969c1e03553a11c0dd44736c46054026333
Eduardo Fraga <eduardo@eftech.com.br>
2024-04-14 15:16:01 -0300

    Update php
    On branch update-php
    Changes to be committed:
    	modified:   README.md
    	modified:   docker/_env
    	modified:   docker/docker-compose-build.yml
    	modified:   docker/docker-compose.yml
    	modified:   docker/nginx/Dockerfile
    	modified:   docker/php/Dockerfile
    	modified:   docker/php/scripts/glpi-db-upgrade.sh
    	new file:   docker/php/scripts/mariadb-timezone.sh
    	new file:   kubernetes/mariadb-job.yaml
    	modified:   kubernetes/mariadb-secret.yaml

---
+++ docker/php/scripts/mariadb-timezone.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# Requires root
+
+mysql -h $MARIADB_HOST -u root -p$MARIADB_ROOT_PASSWORD -e "GRANT SELECT ON \`mysql\`.\`time_zone_name\` TO 'glpi'@'%'; FLUSH PRIVILEGES; FLUSH PRIVILEGES;"
+
