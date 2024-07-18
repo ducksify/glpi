da3f220359eb92a3f86538871afc5b6379153e84
Eduardo Fraga <eduardo@eftech.com.br>
2024-07-18 03:11:02 -0300

    On branch patch-20240718 	modified:   docker/php/Dockerfile 	modified:   docker/php/scripts/mariadb-timezone.sh

--- docker/php/scripts/mariadb-timezone.sh
+++ docker/php/scripts/mariadb-timezone.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-# Requires root
+# Requires root user
 
 mariadb -h "$MARIADB_HOST" -u root -p"$MARIADB_ROOT_PASSWORD" -e "\"GRANT SELECT ON \`mysql\`.\`time_zone_name\` TO 'glpi'@'%'; FLUSH PRIVILEGES;\""
 
